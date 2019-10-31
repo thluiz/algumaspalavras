7776107579ceb0b49ccd9c0e6dc4b8a3a82769ec
Thiago Luiz Silva <contato@thluiz.com>
2019-10-31 15:33:36 -0300

    updating onServiceWorkerUpdateReady => onServiceWorkerUpdateFound

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-export const onServiceWorkerUpdateReady = () => {
+export const onServiceWorkerUpdateFound = () => {
     const answer = window.confirm(
         `Surgiram mais algumas palavras... ` +
         `Deseja atualizar a página para obter a última versão?`
