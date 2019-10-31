1b6d49cc1bedc33f0f0cc94363de295b4446c030
Thiago Luiz Silva <contato@thluiz.com>
2019-10-31 15:29:43 -0300

    adding update o content refresh

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+        `Surgiram mais algumas palavras... ` +
+        `Deseja atualizar a página para obter a última versão?`
+    )
+
+    if (answer === true) {
+        window.location.reload()
+    }
+}
